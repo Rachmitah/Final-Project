69421cdc5f90581011d7d3353ee2519cae69d958
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2018-06-01 22:29:43 +0700

    nambah scanning

---
+++ array.py
@@ -0,0 +1,36 @@
+import numpy as np
+from ultra_man import get_range
+
+arr = np.array([
+    [1,   2,  3,  4,  5,  6],	# 1
+    [12, 11, 10,  9,  8,  7],	# 2
+    [13, 14, 15, 16, 17, 18],	# 3
+    [24, 23, 22, 21, 20, 19]])	# 4
+
+hsl = np.array([
+    [0, 0, 0, 0, 0, 0],	# 1
+    [0, 0, 0, 0, 0, 0],	# 2
+    [0, 0, 0, 0, 0, 0],	# 3
+    [0, 0, 0, 0, 0, 0]])# 4
+
+n = 1
+
+for y in hsl:
+	# Y Ganjil -> dari kiri ke kanan
+	if (n == 1):
+		for x in y:
+			# lakukan scan ultrasonic
+			hsl[y][x] = get_range()
+			print(x)
+			time.sleep(1)
+		n = 0
+	# Y Genap -> dari kanan ke kiri
+	else:
+		for x in reversed(y):
+			# lakukan scan ultrasonic
+			hsl[y][x] = get_range()
+			print(x)
+			time.sleep(1)
+		n = 1
+
+print(arr[0][1])
