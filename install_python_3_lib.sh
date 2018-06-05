baddd96692d667e9fa0f81bceb39c4a61c5c68c1
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2018-06-05 14:31:57 +0700

    Update install_python_3_lib.sh

--- install_python_3_lib.sh
+++ install_python_3_lib.sh
@@ -1,5 +1,29 @@
 clear
 echo "======================================"
+echo "            Update Library            "
+echo "======================================"
+sudo apt-get update
+sudo apt-get upgrade
+echo "======================================"
+echo "              Pre Quest               "
+echo "======================================"
+sudo apt-get install build-essential 
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install pkg-config
+sudo apt-get install libjpeg8-dev 
+sudo apt-get install libtiff4-dev 
+sudo apt-get install libjasper-dev 
+sudo apt-get install libpng12-dev
+sudo apt-get install libavcodec-dev
+sudo apt-get install libavformat-dev
+sudo apt-get install libswscale-dev
+sudo apt-get install libv4l-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install gfortran
+sudo apt-get install python3-dev
+echo "======================================"
 echo "           INSTALL LIBRARY            "
 echo "======================================"
 python3 -m pip install numpy
